8f258a66e8d9b1ca8b0af9ad05918d9ae7c206c3
Ronddaa <tronddaa@gmail.com>
2025-05-24 23:16:11 +0200

    fix proposal position

--- src/components/Proposal/Proposal.jsx
+++ src/components/Proposal/Proposal.jsx
@@ -1,5 +1,5 @@
 import styles from "./Proposal.module.css";
-import sprite from "../icons.svg";
+// import sprite from "../icons.svg";
 import CountdownTimer from "../Timer/Timer";
 import { useTranslation } from "react-i18next";
 
@@ -12,33 +12,23 @@ export default function Proposal() {
         <h2 className={styles.titleProposal}>{t("proposalSection.title")}</h2>
         <ul className={styles.wrapperProposal}>
           <li>
-            <svg className={styles.iconStar} width={13} height={13}>
-              <use xlinkHref={`${sprite}#icon-star`}></use>
-            </svg>
+            
             <p className={styles.bigText}>{t("proposalSection.list1")}</p>
           </li>
           <li>
-            <svg className={styles.iconStar} width={13} height={13}>
-              <use xlinkHref={`${sprite}#icon-star`}></use>
-            </svg>
+            
             <p className={styles.lowText}>{t("proposalSection.list2")}</p>
           </li>
           <li>
-            <svg className={styles.iconStar} width={13} height={13}>
-              <use xlinkHref={`${sprite}#icon-star`}></use>
-            </svg>
+            
             <p className={styles.bigText}>{t("proposalSection.list3")}</p>
           </li>
           <li>
-            <svg className={styles.iconStar} width={13} height={13}>
-              <use xlinkHref={`${sprite}#icon-star`}></use>
-            </svg>
+            
             <p className={styles.lowText}>{t("proposalSection.list4")}</p>
           </li>
           <li>
-            <svg className={styles.iconStar} width={13} height={13}>
-              <use xlinkHref={`${sprite}#icon-star`}></use>
-            </svg>
+            
             <p className={styles.bigText}>{t("proposalSection.list5")}</p>
           </li>
         </ul>
